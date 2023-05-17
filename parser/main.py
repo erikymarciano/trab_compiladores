1e96b34ad2b246274f35ca417302f1f4adf4131c
Eriky <erikymarciano@id.uff.br>
2023-05-17 07:05:46 -0300

    adicionado logica para criação da arvore sintatica

--- parser/main.py
+++ parser/main.py
@@ -1,28 +1,53 @@
 from parser_data import *
+from bnf import *
 
 simple_input = [
- ['int', 'int'], 
- ['identifier', 'main'],
- ['(', '('],
- [')', ')'], 
- ['{', '{'],
- ['int', 'int'],
- ['identifier', 'a'],
- ['='],
- ['number', '5'],
- [';', ';']
+ ['int', 'int', 1], 
+ ['identifier', 'main', 2],
+ ['(', '(', 3],
+ [')', ')', 4], 
+ ['{', '{', 5],
+ ['int', 'int', 6],
+ ['identifier', 'a', 7],
+ ['=', '=', 8],
+ ['number', '5', 9],
+ [';', ';', 10],
+ ['$', '$', 11]
 ]
 
-pile = []
-pile.extend(bnf_rules[0][::-1])
 
-for token in simple_input:
-    if pile[-1] not in terminals:
-        aux = pile[-1]
-        pile.pop()
-        pile.extend()
-    elif token == pile[-1]:
-        continue
-    else:
-        print('Erro na linha x')
+
+def parser(scanner_output):
+    pile = ['$']
+    pile.extend(bnf_rules[0][::-1])
+    print(pile)
+
+    for i in range(len(scanner_output)):
+        while pile[-1] != '$' and scanner_output[i] != '$':
+            if (pile[-1] in terminals) and (pile[-1] == scanner_output[i][0]):
+                # match();
+                pile.pop()
+                print(pile)
+                i = i+1
+            else:
+                print('table['+pile[-1]+']['+scanner_output[i][0]+']')
+                aux = table[pile[-1]][scanner_output[i][0]]
+                print(aux)
+                if (pile[-1] not in terminals) and (aux != None):
+                    # producao()
+                    pile.pop()
+                    print(aux[0])
+                    pile.extend(bnf_rules[aux[0]-1][::-1])
+                    print(pile)
+                else:
+                    print('Erro na linha ' + str(scanner_output[3]))
+                    break
+        if pile[-1] == '$' and scanner_output[i] == '$':
+            print('arvore')
+        else:
+            print('Erro na linha ' + str(scanner_output[3]))
+            break
+
+
+parser(simple_input)
 
